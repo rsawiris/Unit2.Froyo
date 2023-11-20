5415d2c6d10bd62499290dcb36b62a5bdc6fc141
Rafael Sawiris <rafaelsawiris@Rafaels-MacBook-Pro.local>
2023-11-19 17:00:10 -0800

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const flavors = window.prompt ("Enter a list of froyo flavors", "vanilla,vanilla,vanilla,strawberry,coffee,coffee")
+console.log(flavors)
+const flavorsArr = flavors.split(',')
+console.log(flavorsArr)
+
+function countFlavors(flavors){
+    const output = []
+    for(i = 0; i < flavorsArr.length; i++) {
+        if (flavors[flavor] === undefined) 
+        return flavors
+    }
+}
+
+const numFlavors = countFlavors(flavors)
+
+console.log(numFlavors)
+
+/* I'm honestly not sure how to do this one. I tried to review the
+    the slides over the weekend and redo the workshop but it's still
+    a little confusing. */
+
+    
